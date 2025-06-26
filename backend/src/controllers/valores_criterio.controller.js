57a22299034b79a3ceb857a2b7f7452179c2e224
breyesr <breyessrr@gmail.com>
2025-06-26 09:38:26 -0400

    Backend routes centralization

---
+++ backend/src/controllers/valores_criterio.controller.js
@@ -0,0 +1,16 @@
+export const obtenerValorCriterios = async(req, res) => {
+    res.send("Obteniendo Criterio")
+}
+
+export const crearValorCriterio = async(req, res) => {
+    res.send("Creando Criterio")
+}
+
+export const actualizarValorCriterio = async(req, res) => {
+    res.send("Actualizando Criterio")
+}
+
+export const eliminarValorCriterio = async(req, res) => {
+    res.send("Eliminando Criterio")
+}
+
